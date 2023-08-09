78887bbc611b1f206349e4761630b9aee44781c7
luhvailante1234 <105363292+luhvailante1234@users.noreply.github.com>
2023-08-09 11:12:05 -0300

    NovoCódigo
    NovoCódigo_Luiza

--- Grafico.py
+++ Grafico.py
@@ -1,35 +1,35 @@
- def desenha_linha_equipotencial(self, x, y):
+import matplotlib.pyplot as plt
 
-        # Calcula o potencial do lugar clicado
-        potencial_alvo = Carga.calcula_potencial(x, y, self.cargas)
+from Carga import Carga
+from Constantes import Constantes
 
-        # Configura a linha de contorno
-        levels = np.linspace(potencial_alvo - 1, potencial_alvo + 1, Constantes.pontos())
 
-        # Calcula os valores de potencial em cada ponto do gráfico
-        for i in range(Constantes.pontos()):
-            for j in range(Constantes.pontos()):
-                for carga in self.cargas:
-                    temp_x = self.X[i, j] - carga.x
-                    temp_y = self.Y[i, j] - carga.y
-                    distancia = np.sqrt(temp_x**2 + temp_y**2)
-                    self.VALORES_Q[i, j] += Constantes.constanteEletrostatica() * carga.q / distancia
+class Grafico:
+    def __init__(self):
+        self.cargas = [] # Lista de cargas
+        
+        self.grid = np.linspace(-40, 40, Constantes.pontos())
 
-        # Faz a linha equipotencial com base nos valores iguais ao potencial do ponto clicado
-        plt.contour(self.X, self.Y, self.VALORES_Q, levels=levels, colors='grey')
+        '''
+        Cria uma grade que vai de -30 até 30 que
+        pode ser alterada a depdender da posição
+        das cargas ou da posição do clique.
+        
+        Estes valores serão utilizados para calcular
+        o potencial em cada ponto do gráfico.
+        '''
+        self.X, self.Y = np.meshgrid(self.grid, self.grid)
+        '''
+        Recebe o mesmo valor de X e Y, mas substitui por zeros.
+        
+        O valor de cada posição deste array será utilizado
+        para desenhar a equipotencial.
+        '''
+        self.VALORES_Q = np.zeros_like(self.X)
 
-    def atualiza_grafico(self, x, y):
-        plt.clf()
+    def desenha_grafico(self, x, y):
+        potencial = Carga.calcula_potencial(x, y, self.cargas)
+        plt.title(f"Potencial elétrico: {potencial:.2f} Volts")
+        plt.scatter(x, y, s=200, marker='o', c='purple')
+        plt.text(x, y, f"({x:.2f}, {y:.2f})", ha='center', va='bottom')
 
-        for carga in self.cargas:
-            plt.scatter(carga.x, carga.y, s=200, marker='o', c='pink')
-            plt.text(carga.x, carga.y, f"{carga.q} C", ha='center', va='center')
-
-        self.desenha_linha_equipotencial(x, y)
-
-        self.desenha_grafico(x, y)
-
-        plt.xlabel("Coordenada x (m)")
-        plt.ylabel("Coordenada y (m)")
-        plt.grid()
-        plt.draw()
